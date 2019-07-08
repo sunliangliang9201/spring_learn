690805b993aa8fac9f763151a02604ad805e5067
sunliangliang <397585361@qq.com>
2019-07-08 14:43:38 +0800

    init

---
+++ mybatis_test_01/src/main/java/com/sunll/mybatis/po/User.java
@@ -0,0 +1,69 @@
+package com.sunll.mybatis.po;
+
+import java.util.Date;
+
+/**
+ * desc
+ *
+ * @author sunliangliang
+ * @date 2019/7/8 12:13
+ */
+public class User {
+    private int id;
+    private String username;
+    private String sex;
+    private Date birthday;
+    private String address;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", sex='" + sex + '\'' +
+                ", birthday=" + birthday +
+                ", address='" + address + '\'' +
+                '}';
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+}
