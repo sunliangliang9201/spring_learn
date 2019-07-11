ff12e5d48f7936091a3020856d6295571835d6cd
sunliangliang <397585361@qq.com>
2019-07-11 22:14:08 +0800

    init

--- ssm01/src/main/java/com/sunll/ssm/controller/UploadController.java
+++ ssm01/src/main/java/com/sunll/ssm/controller/UploadController.java
@@ -1,5 +1,8 @@
 package com.sunll.ssm.controller;
 
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.WebResource;
+import com.sunll.ssm.utils.Commons;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
@@ -23,26 +26,46 @@ public class UploadController {
 
     @RequestMapping("uploadPic")
     public void uploadPic(HttpServletRequest request, String fileName, PrintWriter out){
-        //把request墙砖为多部件请求对象
+        //把Request强转成多部件请求对象
         MultipartHttpServletRequest mh = (MultipartHttpServletRequest) request;
         //根据文件名称获取文件对象
         CommonsMultipartFile cm = (CommonsMultipartFile) mh.getFile(fileName);
         //获取文件上传流
         byte[] fbytes = cm.getBytes();
 
-        //文件名称在服务器有可能重复
-        String newFileName = "";
+        //文件名称在服务器有可能重复？
+        String newFileName="";
         SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddHHmmssSSS");
         newFileName = sdf.format(new Date());
-        Random r = new Random();
-        for(int i = 0; i < 3; i++){
-            newFileName = newFileName + r.nextInt(10);
-        }
-        //获取文件扩展名
-        String originalFileName = cm.getOriginalFilename();
-        String suffix = originalFileName.substring(originalFileName.lastIndexOf("."));
 
-        //创建jersey服务器，进行跨服务器上传
+        Random r = new Random();
+
+        for(int i =0 ;i<3;i++){
+            newFileName=newFileName+r.nextInt(10);
+        }
+
+        //获取文件扩展名
+        String originalFilename = cm.getOriginalFilename();
+        String suffix = originalFilename.substring(originalFilename.lastIndexOf("."));
+
+        //创建jesy服务器，进行跨服务器上传
+        Client client = Client.create();
+        //把文件关联到远程服务器
+        WebResource resource = client.resource(Commons.PIC_HOST+"/upload/"+newFileName+suffix);
+        //上传
+        resource.put(String.class, fbytes);
+
+
+        //ajax回调函数需要会写写什么东西？
+        //图片需要回显：需要图片完整路径
+        //数据库保存图片的相对路径.
+        String fullPath = Commons.PIC_HOST+"/upload/"+newFileName+suffix;
+
+        String relativePath="/upload/"+newFileName+suffix;
+        //{"":"","":""}
+        String result="{\"fullPath\":\""+fullPath+"\",\"relativePath\":\""+relativePath+"\"}";
+
+        out.print(result);
 
     }
 }
--- ssm01/src/main/webapp/WEB-INF/jsps/itemsList.jsp
+++ ssm01/src/main/webapp/WEB-INF/jsps/itemsList.jsp
@@ -2,7 +2,7 @@
     pageEncoding="UTF-8" isELIgnored="false" %>
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <%@ taglib uri="http://java.sun.com/jsp/jstl/fmt"  prefix="fmt"%>
-<c:set var="picPath" value="http://127.0.0.1:8003/ssmImage19"></c:set>
+<c:set var="picPath" value="http://127.0.0.1:8081"></c:set>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
